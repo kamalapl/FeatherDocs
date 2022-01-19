fae1c7ecc7058386170d3da84e11d187e5615b17
Gopika Meena S <gopikameena.s@gmail.com>
2022-01-19 21:02:25 +0530

    initial commit

---
+++ server/server.py
@@ -0,0 +1,13 @@
+import socketio
+
+sio=socketio.Server()
+
+app=socketio.WSGIApp(sio)
+
+@sio.event
+def connect(sid,environ):
+    print(sid,"connected")
+
+@sio.event
+def disconnect(sid, environ):
+    print(sid,"disconnected")
